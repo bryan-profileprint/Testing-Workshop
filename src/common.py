753747c151700ad5d97ff5d294add5250268f6ec
bp <>
2022-06-03 09:56:23 +0800

    Remove stuff

--- src/common.py
+++ src/common.py
@@ -1,5 +1,5 @@
 def sum_two(x1, x2):
-    return (x1) + (x2)
+    raise NotImplementedError
 
 def mult_two(x1, x2):
-    return x1*x2
+    raise NotImplementedError
--- src/words.py
+++ src/words.py
@@ -1,18 +1,33 @@
 import re
 
-def get_word_count(content):
-    content = content.lower()
-    words = re.findall(r"\w+", content)
-    word_count = {}
-    for word in words:
-        if word not in word_count.keys():
-            word_count[word] = 0
-        word_count[word] += 1
-    return word_count
+def get_word_count(content: str):
+    """
+    Takes a text string as input and returns a dictionary where keys are unique words in lowercase and values are 
+    the number of occurences in the text
+    
+    e.g 
+    >> get_word_count("Hello World")
+    {'hello': 1, 'world': 1}
+    """
+    raise NotImplementedError
 
-def get_top_word(content):
-    word_count = get_word_count(content)
-    max_count = word_count.values()
+def get_top_word(content: str):
+    """
+    Takes a text string as input and returns the words that occurs the most in a list.
+    
+    e.g 
+    >> get_top_word("Hello hello the World world")
+    ["hello", "world"]
+    """
+    raise NotImplementedError
 
-    return [word for word, count in word_count.items()
-            if count == max_count]
+def generate_text_report(content: str):
+    """
+    Takes a text string as input and returns a report in the following format.
+    "There are <X> unique words and the top occuring words is/are <word1>, <word2>"
+    
+    e.g 
+    >> generate_text_report("Hello and hello to the world")
+    "There are 5 unique words and the top occuring words is/are 'hello'"
+    """
+    raise NotImplementedError
